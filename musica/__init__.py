286522dd8a4e62f2b518b488df7e88d59e045bbb
MatiasDeVincenti <mdevinceti@etec.uba.ar>
2024-05-20 12:12:53 -0300

    creè la pagina

--- musica/__init__.py
+++ musica/__init__.py
@@ -21,3 +21,15 @@ def artistas():
    lista_artistas = res.fetchall()
    pagina = render_template('artista.html',artistas=lista_artistas)
    return pagina
+
+@app.route('/canciones')
+def canciones():
+   consulta = """
+       SELECT Name, Milliseconds, Composer FROM tracks;
+    """
+
+   con = db.get_db()
+   res = con.execute(consulta)
+   lista_canciones = res.fetchall()
+   pagina = render_template('canciones.html',canciones=lista_canciones)
+   return pagina
